7a914fb080a2def9a3a6487beb0181f34976de65
Noah Graff <graffixgames@gmai.com>
2017-11-10 10:26:08 -0500

    Added Game Class

---
+++ Ultimate Tic-Tac-Toe/Game.swift
@@ -0,0 +1,17 @@
+//
+//  Game.swift
+//  Ultimate Tic-Tac-Toe
+//
+//  Created by  on 11/10/17.
+//  Copyright © 2017 Hi-Crew. All rights reserved.
+//
+
+import Foundation
+
+class Game {
+	private var board = [ [0, 0, 0], [0, 0, 0], [0, 0, 0] ]
+	enum Plr {
+		case x
+		case o
+	}
+}
